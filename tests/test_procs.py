a8958d6817281782041975cf3ed5f3fa1448256d
Harry <hjwp2@cantab.net>
2014-11-04 08:52:44 +0000

    Test for single Process run

--- tests/test_procs.py
+++ tests/test_procs.py
@@ -1,4 +1,9 @@
 from procs import Process
 
-def test_test_runner():
-    assert False
+
+def test_single_proc():
+    ls = Process('ls')
+    ls.run()
+    assert ls.returncode == 0
+    assert ls.stdout == 'file1 file2 file3'
+
